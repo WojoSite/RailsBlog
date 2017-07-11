602d2d2ff6cd6558d4adf2d3a10844c6e7d6df2b
Wojo <matt.wojtkun@gmail.com>
2017-07-11 10:08:52 -0400

    added validations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -60,6 +60,7 @@ class UsersController < ApplicationController
   # GET /users/new
   def new
     @user = User.new
+    @address = Address.new
   end
 
   # GET /users/1/edit
@@ -70,10 +71,13 @@ class UsersController < ApplicationController
   # POST /users.json
   def create
     @user = User.new(user_params)
-
+    puts "**** params[:user].inspect #{params[:user].inspect}"
+   puts "**** params[:address][:post_id] #{params[:address][:street]}"
     respond_to do |format|
       if @user.save
         session[:user_id] = @user.id
+        @address = Address.new(address_params)
+        puts "\n ===PARAMS: #{params.inspect}==="
         format.html { redirect_to "/blogfeed", notice: 'User was successfully created.' }
         format.json { render :show, status: :created, location: @user }
       else
@@ -126,5 +130,11 @@ class UsersController < ApplicationController
     def user_params
       puts "******* user_params *******"
       params.require(:user).permit(:username, :password, :password_confirmation, :firstname, :lastname, :email, :imgsrc)
+    end
+
+    def address_params
+
+      puts "******* address_params *******"
+      params.require(:address).permit(:user_id, :street, :city, :state, :zip)
     end
 end
